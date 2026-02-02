89a53eb07ab361b160e73eb68e5a8ebd8e0e0896
Graham McIntire <graham@mcintire.me>
2026-02-02 17:03:46 -0600

    fix snmp community redaction

--- src/websocket_client.rs
+++ src/websocket_client.rs
@@ -347,7 +347,7 @@ fn redact_community(community: &str) -> String {
     if len <= 2 {
         "**".to_string()
     } else {
-        format!("{}***", &community[..2])
+        format!("{}**", &community[..2])
     }
 }
 
@@ -1039,7 +1039,7 @@ mod tests {
 
     #[test]
     fn test_redact_community_normal() {
-        assert_eq!(redact_community("public"), "pu***");
+        assert_eq!(redact_community("public"), "pu**");
     }
 
     #[test]
@@ -1055,11 +1055,11 @@ mod tests {
 
     #[test]
     fn test_redact_community_three_chars() {
-        assert_eq!(redact_community("abc"), "ab***");
+        assert_eq!(redact_community("abc"), "ab**");
     }
 
     #[test]
     fn test_redact_community_long() {
-        assert_eq!(redact_community("mysecretcommunity"), "my***");
+        assert_eq!(redact_community("mysecretcommunity"), "my**");
     }
 }
