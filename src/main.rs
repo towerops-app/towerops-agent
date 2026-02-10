7bffbecd21da381c5566c790a3e2567dcc862b4b
Graham McIntire <graham@mcintire.me>
2026-02-10 15:44:15 -0600

    Fix function_casts_as_integer warnings in crash handler
    Cast function pointers through *const () before converting to
    sighandler_t, as required by Rust 1.93+.

--- src/main.rs
+++ src/main.rs
@@ -141,9 +141,9 @@ fn install_crash_handler() {
             }
         }
 
-        libc::signal(libc::SIGSEGV, crash_handler as libc::sighandler_t);
-        libc::signal(libc::SIGBUS, crash_handler as libc::sighandler_t);
-        libc::signal(libc::SIGABRT, crash_handler as libc::sighandler_t);
+        libc::signal(libc::SIGSEGV, crash_handler as *const () as libc::sighandler_t);
+        libc::signal(libc::SIGBUS, crash_handler as *const () as libc::sighandler_t);
+        libc::signal(libc::SIGABRT, crash_handler as *const () as libc::sighandler_t);
     }
 }
 
